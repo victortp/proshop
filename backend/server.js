e67e50aad3ff403e7a53f20afa12d736f0f982b4
Victor <teixeirapinheiroster@gmail.com>
2021-01-10 19:56:15 -0300

    product reviews

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors';
+import morgan from 'morgan';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
@@ -12,6 +13,10 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 dotenv.config();
 connectDB();
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan());
+}
 
 app.get('/', (req, res) => {
   res.send('API is running...');
